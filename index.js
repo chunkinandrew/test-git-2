64845838a07ac3f2e7f95c85997d8bd77f1601f0
Andrei Chunkin <chunkinandrew@gmail.com>
2025-12-02 19:06:22 +0300

    123

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 console.log('Hello World')
 
-<<<<<<< HEAD
 const userage = 35
 if(userage >= 18) {
     console.log("Вы совершеннолетний")
 } else {
     console.log("Вы НЕ совершеннолетний")
-=======
-const age =20
-if(age>=18) {
-    console.log('Вы совершеннолетний')
-} else {
-    console.log('Вы НЕ совершеннолетний')
->>>>>>> feature/user-access
 }
